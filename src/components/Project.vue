17c7f3bcdd3dedf653e558b3b092db8fdee352cb
willyb321 <wbwilliam7@gmail.com>
2017-06-18 20:38:30 +1000

    format and add br between stars and link

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -13,13 +13,10 @@
 				<div class="box">
 					<div class="container">
 						<h1 class="title">{{ item.name }}</h1>
-						<h2 v-if="item.description" class="subtitle">{{ item.description }} <span v-if="item.stargazers_count > 0">({{ item.stargazers_count }} 🌟)</span><a target="_blank" :href="item.html_url">({{ item.full_name }})</a></h2>
-						<!--<a v-if="item.doc.origlink" class="button is-info" style="float: right;" :href="item.doc.origlink" target="_blank">View Story</a>-->
-						<!--<a v-else="" class="button is-info" style="float: right;" :href="item.doc.link" target="_blank">View Story</a>-->
-						<!--<input onclick="this.value=this.value=='Show Summary'?'Hide Summary':'Show Summary';" value='Show Summary' type='button' class="is-primary is-bold button spoilerbutton">-->
-						<!--<div class="spoiler">-->
-							<!--<div>{{ post.docs[index].doc.description.replace(/<[^>]+>/g, '') }}</div>-->
-						<!--</div>-->
+						<h2 v-if="item.description" class="subtitle">{{ item.description }}
+							<span v-if="item.stargazers_count > 0">({{ item.stargazers_count }} 🌟)</span><br>
+							<a target="_blank" :href="item.html_url">{{ item.full_name }}</a>
+						</h2>
 					</div>
 				</div>
 				<hr>
@@ -28,33 +25,33 @@
 	</div>
 </template>
 <script>
-	import Vue from 'vue'
+import Vue from 'vue'
 
-	export default {
-		data() {
-			return {
-				loading: false,
-				post: null,
-				error: null
-			}
-		},
-		created() {
-			// fetch the data when the view is created and the data is
-			// already being observed
-			this.fetchData()
-		},
-		watch: {
-			// call again the method if the route changes
-			'$route': 'fetchData'
-		},
-		methods: {
-			async fetchData() {
-				this.error = this.post = null;
-				this.loading = true;
-				// replace getPost with your data fetching util / API wrapper
-				this.post = require('../projects.json');
-				this.loading = false;
-			}
+export default {
+	data() {
+		return {
+			loading: false,
+			post: null,
+			error: null
+		}
+	},
+	created() {
+		// fetch the data when the view is created and the data is
+		// already being observed
+		this.fetchData()
+	},
+	watch: {
+		// call again the method if the route changes
+		'$route': 'fetchData'
+	},
+	methods: {
+		async fetchData() {
+			this.error = this.post = null;
+			this.loading = true;
+			// replace getPost with your data fetching util / API wrapper
+			this.post = require('../projects.json');
+			this.loading = false;
 		}
 	}
+}
 </script>
