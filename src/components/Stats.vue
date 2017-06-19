f79d5240ea091d0eed9f3468a2a55910d1a5caff
willyb321 <wbwilliam7@gmail.com>
2017-06-19 12:37:19 +1000

    stuff

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,40 +1,43 @@
 <template>
-<div>
-	<div class="loading" v-if="loading">
-		Loading...
-	</div>
+	<div>
+		<div class="loading" v-if="loading">
+			Loading...
+		</div>
 
-	<div v-if="error" class="error">
-		{{ error }}
-	</div>
-	<div class="post">
-		<div v-if="post">
-			<div style="width: 25%" class="card">
-				<header class="card-header">
-					<p class="card-header-title">
-						Some statistics from Github
-					</p>
-					<a class="card-header-icon">
-						<span class="icon">
-							<i class="fa fa-angle-down"></i>
-						</span>
-					</a>
-				</header>
-				<div class="card-content">
-					<div class="content">
-						<span class="tag">
-							<i class="fa fa-map-marker" aria-hidden="true"> </i> {{ post.location }}</span>
-						<span class="tag">
-							<i class="fa fa-question" aria-hidden="true"> </i> {{ post.bio }}</span>
-						<span class="tag">
-							<i class="fa fa-github" aria-hidden="true"> </i> {{ post.public_repos }} public repos</span>
-						<span class="tag">
-							<i class="fa fa-sticky-note" aria-hidden="true"> </i> {{ post.public_gists }} public gists</span>
+		<div v-if="error" class="error">
+			{{ error }}
+		</div>
+		<div class="post">
+			<div v-if="post">
+				<div style="width: 25%" class="card">
+					<header class="card-header">
+						<p class="card-header-title">
+							Some statistics from Github
+						</p>
+						<a class="card-header-icon">
+							<span class="icon">
+								<i class="fa fa-angle-down"></i>
+							</span>
+						</a>
+					</header>
+					<div class="card-content">
+						<div class="content">
+							<span class="tag">
+								<i class="fa fa-map-marker" aria-hidden="true"> </i> {{ post.location }}</span>
+							<br>
+							<span class="tag">
+								<i class="fa fa-question" aria-hidden="true"> </i> {{ post.bio }}</span>
+							<br>
+							<span class="tag">
+								<i class="fa fa-github" aria-hidden="true"> </i> {{ post.public_repos }} public repos</span>
+							<br>
+							<span class="tag">
+								<i class="fa fa-sticky-note" aria-hidden="true"> </i> {{ post.public_gists }} public gists</span>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 	</div>
 </template>
 <script>
